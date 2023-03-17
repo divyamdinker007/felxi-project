a32b13171966a584077350ae30f626e3f2b2de0e
divyamdinker007 <71220418+divyamdinker007@users.noreply.github.com>
2023-03-17 13:26:26 +0530

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: Login
   },
   {
     path: '/about',
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="center">
+        <h1>Login page</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
