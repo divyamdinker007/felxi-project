8ccdbc75f8515c6004068d29bc036522f0e1bf4e
divyamdinker007 <71220418+divyamdinker007@users.noreply.github.com>
2023-03-17 14:17:34 +0530

    second commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="center">
+        <v-row>
+            <v-col cols="10" offset="1">
+                <v-avatar color="primary" size="56" tile>
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+            </v-col>
+        </v-row>
         <h1>Login page</h1>
 
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
+    
+<style lang="scss" scoped></style>
