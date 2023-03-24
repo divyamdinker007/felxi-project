fef81463b9f8d709ebd0945619e73e0dd525513c
divyamdinker007 <71220418+divyamdinker007@users.noreply.github.com>
2023-03-24 13:11:17 +0530

    initial commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,34 @@
 <template>
     <div class="center">
         <v-row>
-            <v-col cols="10" offset="1">
+            <v-col cols="10" offset="1" sm="6" offset-sm="3" md="4" offset-md="4">
                 <v-avatar color="primary" size="56" tile>
                     <v-img src="@/assets/logo.png"></v-img>
                 </v-avatar>
+                <v-text-field label="Username" type="text" v-model="username"></v-text-field>
+            <v-text-field label="Passwrod" type="password" v-model="password"></v-text-field>
+                <v-btn class="base-inverted" block @click="authenticate()">Login</v-btn>
             </v-col>
         </v-row>
-        <h1>Login page</h1>
+
 
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        authenticate() {
+            alert("Login Happy :)")
+            
+        }
+    },
 }
 </script>
     
